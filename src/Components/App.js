a9539a0863e8b7ba161a768b465be45a7f7783eb
Manan Surana <59257437+suranamanan@users.noreply.github.com>
2020-06-16 21:23:56 -0400

    Parent/Child Components

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,7 +3,10 @@ import React from 'react';
 class App extends React.Component {
   render(){
     return(
-      <h1>Hello World</h1>
+      <div>
+        <h1>Hello World</h1>
+      </div>
+
     )
   }
 }
